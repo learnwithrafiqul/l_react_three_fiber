434b1c61215f952c8e7a8dd6a00ee8eab3e0f695
rafiqul-altersense <rafiqul.altersense@gmail.com>
2023-08-28 17:05:18 +0600

    working

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ const App = () => {
     <div>
       <Canvas id="three-canvas-container">
         <ambientLight args={["#ffffff", 1]} />
-        {/* <pointLight position={[10, 10, 10]} /> */}
         <mesh>
           <sphereGeometry args={[1, 32, 32]} />
           <meshStandardMaterial color="#ffffff" />
